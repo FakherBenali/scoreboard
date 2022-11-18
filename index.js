6a7241d3d0de47b62fc6077184997f3694227587
FakherBenali <fvrbntrl@gmail.com>
2022-11-18 21:22:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let home = document.getElementById("home-score")
+let away = document.getElementById("away-score")
+let home1 = 0
+let away1 = 0
+function plus_oneh(){
+    home1 += 1
+    home.innerText = home1
+   
+}
+function plus_one(){
+    away1 += 1
+    away.innerText = away1
+   
+}
+function plus_twoh(){
+    home1 += 2
+    home.innerText = home1
+   
+}
+
+function plus_two(){
+    away1 += 2
+    away.innerText = away1
+   
+}
+
+function plus_threeh(){
+    home1 += 3
+    home.innerText = home1
+   
+}
+
+function plus_three(){
+    away1 += 3
+    away.innerText = away1
+   
+}
